5f17479ed02c99b53a64d51f378799fee9e10aaa
Abolfazl ASeM <a.semnani12@gmail.com>
2025-03-15 14:07:04 +0330

    panel modiriat

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a447e045bc2e393bf91b225a812d5bdc5b73cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9f640502e0f0d3737a8966c5fd91bbfc1b727e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
